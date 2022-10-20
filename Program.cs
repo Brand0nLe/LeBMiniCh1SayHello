0d1cfef6c1ec9fef62f4adce87371c0f376789dd
Brand0nLe <93brandonle@gmail.com>
2022-10-20 08:02:39 -0700

    Final commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿//Cuong Brandon Le
-//10-18-22
+//10-20-22
 //Mini Challenge 1 Say Hello
+//Project will store the user's input for their name and print it back out to them in the terminal
+//If user enters in numbers it will display a message to have them enter in their real name.
+//Has the ability to play again.
 
 
 string userInput = "";
