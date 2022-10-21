e03c72986c845a7b391aa3ef0620dda1e4fc21e7
Brand0nLe <93brandonle@gmail.com>
2022-10-20 19:06:31 -0700

    Final Edit just checking over code.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
 //Project will store the user's input for their name and print it back out to them in the terminal
 //If user enters in numbers it will display a message to have them enter in their real name.
 //Has the ability to play again.
-
+//Peer Review: Madeline Gowan - I like the program! It runs as intended, and I appreciate the compliment on how beautiful 
+//my name is lol. I also like the fact that Ken is then included into the conversation. I was a bit confused when it looped, 
+//but I now understand that when it loops, it's because you said you have time to talk to Ken.
 
 string userInput = "";
 string userInputTwo = "";
